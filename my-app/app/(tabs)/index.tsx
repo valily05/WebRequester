73e9a56ff12e6ed46ff517961bb2de4b28d2b75b
valencia <valencia.hartono101@gmail.com>
2026-02-20 11:25:23 +0700

    add text input

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -1,12 +1,20 @@
-import { View, StyleSheet ,Button} from 'react-native';
+import React from 'react';
+import { View, StyleSheet ,Button , TextInput} from 'react-native';
 
 export default function HomeScreen() {
+  const [text, onChangeText] =React.useState('Enter URL');
   return (
 <View style ={styles.containerColumn}>
   <View style ={styles.containerRow}>
+    <TextInput
+    style ={styles.input}
+    onChangeText={onChangeText}
+    value = {text}
+    />
+
     <Button
     title ="Click me"
-    onPress={()=>alert("Stop pressing my button!")}/>
+    onPress={()=>alert("Stop pressing my button!" + text)}/>
   </View>
 </View>
   );
@@ -22,6 +30,10 @@ const styles = StyleSheet.create({
   containerRow : {
     flexDirection:'row',
     justifyContent:'space-between'
-  }
+  },
+  input:{
+justifyContent:'center'
+
+  },
   
 });
